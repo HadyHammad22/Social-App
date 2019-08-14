3b3e5a81ad02975c30885a99122c265c3eb7242b
hady <hady97hamad@gmail.com>
2019-08-14 01:22:56 -0700

    Initial Commit

---
+++ SocialApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Hady Hammad on 8/14/19.
+//  Copyright © 2019 Hady Hammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
