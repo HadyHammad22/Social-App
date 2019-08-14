1a7e6db7d7c616da8cdca86c55fbafb8c37c3f99
hady <hady97hamad@gmail.com>
2019-08-14 15:46:48 -0700

    sign in ui custom classes com[lete

---
+++ SocialApp/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialApp
+//
+//  Created by Hady Hammad on 8/14/19.
+//  Copyright © 2019 Hady Hammad. All rights reserved.
+//
+
+import Foundation
+import UIKit
+let SHADOW_GRAY:CGFloat = 120.0 / 255.0
