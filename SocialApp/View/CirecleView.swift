4cd02565ef672056e33840edd077d3688594d3a0
hady <hady97hamad@gmail.com>
2019-08-17 03:13:37 -0700

    feed-ui part1 completed

--- SocialApp/View/CirecleView.swift
+++ SocialApp/View/CirecleView.swift
@@ -1,20 +1,24 @@
 //
-//  FancyView.swift
+//  CirecleView.swift
 //  SocialApp
 //
-//  Created by Hady Hammad on 8/14/19.
+//  Created by Hady Hammad on 8/16/19.
 //  Copyright © 2019 Hady Hammad. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CirecleView: UIImageView{
     override func awakeFromNib() {
         super.awakeFromNib()
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width/2
     }
 }
